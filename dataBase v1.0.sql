a1adb865c858d892a4a93a883c938d1472e789d9
BonatoManuele <51159728+BonatoManuele@users.noreply.github.com>
2020-12-15 16:38:30 +0100

    Add files via upload

---
+++ dataBase v1.0.sql
@@ -0,0 +1,21 @@
+CREATE TABLE user(
+	mail varchar(48) NOT NULL PRIMARY KEY,
+	nome varchar(24) NOT NULL,
+	cognome varchar(24) NOT NULL,
+	password varchar(64) NOT NULL);
+
+CREATE TABLE presepi(
+	mail varchar(48) NOT NULL,
+	fotoPresepe varchar(128) NOT NULL,
+	fotoNatalita varchar(128) NOT NULL,
+	nomePresepe varchar(48) NOT NULL,
+	indirizzo varchar(64) NOT NULL,
+	categoria ENUM('adulti','ragazzi') NOT NULL,
+	descrizione varchar(200) NOT NULL,
+	dataInserimento date NOT NULL,
+	PRIMARY KEY (mail,nomePresepe),
+	FOREIGN KEY(mail) REFERENCES user(mail)
+);
+
+
+	 
