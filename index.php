410b978fbbd356b42af7deb2ed56a5f0d6a74254
Sadi97 <andrea.salmaso@live.it>
2020-12-17 23:57:11 +0100

    Base template, riordino cartelle definitivo

--- index.php
+++ index.php
@@ -1,14 +1,14 @@
 <?php
     require_once("php/templateBuilder.php");
 
-    session_start();
-
-    $builder = new TemplateBuilder("homepage");
-
+    // il parametro in input deve avere lo stesso nome del file che contiene tutto il codice html
+    $builder = new TemplateBuilder("home");
+    $builder->setHead();
+    
     $builder->setHeader();
-    //$builder->setNavbar(file_get_contents(__DIR__."/php/content/_navbar.html"));
-    $builder->setBody(file_get_contents(__DIR__."/php/content/home_body.html"));
+    $builder->setBreadcrumb();
+    $builder->setNavbar();
+    $builder->setFooter();
 
-
-
+    $builder->build();
 ?>
--- php/templateBuilder.php
+++ php/templateBuilder.php
@@ -1,32 +1,83 @@
 <?php
     /* Classe necessaria alla costruzione delle varie pagine */
     class TemplateBuilder {
-        private $pageName;
-        private $navbar;
-        private $body;
+        private $_head;
+        private $_header;
+        private $_breadcrumb;
+        private $_navbar;
+        private $_footer;
+        
+        private $_pageName;
+        private $_whole_page;
+
         
         // costruttore con parametro: prende in input il nome della pagina
-        // utile per la costruzione dell'header
-        public function __construct(string $_pageName) {
-            $this->pageName = $_pageName;
+        public function __construct(string $pageName) {
+            $this->_pageName = $pageName;
+            $this->_whole_page = file_get_contents(__DIR__ . "/content/" . $this->_pageName . ".html");
+        }
+
+        // setter per l'<head>
+        public function setHead() {
+            // recupera il contenuto dell'head (generico)
+            $this->_head = file_get_contents(__DIR__."/content/common/_head.html");
+            
+            // ne cambia il titolo
+            // da creare un caso per ogni pagina
+            if($this->_pageName=="home") {
+                $this->_head = str_replace("{title}", "Concorso Farra di Soligo", $this->_head);
+            }
+
+
+
+
+            
+            // sostituisce il contenuto di <headPH></headPH> (PlaceHolder) in $_whole_page
+            // con quello di $this->_head
+            $this->_whole_page = str_replace("<headPH></headPH>", $this->_head, $this->_whole_page);
         }
 
         // setter per l'header
         public function setHeader() {
-            $this->header = file_get_contents(__DIR__."/content/_header.html");
-            if($this->pageName=="homepage") {
-                $this->header = str_replace("{title}", "Concorso Farra di Soligo", $this->header);
-            }
+            // recupera il contenuto dell'header (generico)
+            $this->_header = file_get_contents(__DIR__."/content/common/_header.html");
+            // sostituisce il contenuto di <headerPH></headerPH> (PlaceHolder) in $_whole_page
+            // con quello di $this->_header
+            $this->_whole_page = str_replace("<headerPH></headerPH>", $this->_header, $this->_whole_page);
         }
-
+        
+        // setter per il breadcrumb
+        public function setBreadcrumb() {
+            // recupera il contenuto del breadcrumb (generico, anche se bisognerà fare in modo che
+            // cambi in automatico in base alla pagina)
+            $this->_breadcrumb = file_get_contents(__DIR__."/content/common/_breadcrumb.html");
+            // sostituisce il contenuto di <breadcrumbPH></breadcrumbPH> (PlaceHolder) in $_whole_page
+            // con quello di $this->_breadcrumb
+            $this->_whole_page = str_replace("<breadcrumbPH></breadcrumbPH>", $this->_breadcrumb, $this->_whole_page);
+        }
+        
         // setter per la navbar/menu
-        public function setNavbar(string $_navbar) {
-            $this->navbar = $_navbar;
+        public function setNavbar() {
+            // recupera il contenuto della navbar (generico, anche se bisognerà fare in modo che
+            //si disattivi in automatico il link alla pagina corrente)
+            $this->_navbar = file_get_contents(__DIR__."/content/common/_navbar.html");
+            // sostituisce il contenuto di <navbarPH></navbarPH> (PlaceHolder) in $_whole_page
+            // con quello di $this->_navbar
+            $this->_whole_page = str_replace("<navbarPH></navbarPH>", $this->_navbar, $this->_whole_page);
         }
 
-        // setter per il body
-        public function setBody(string $_body) {
-            $this->body = $_body;
+        // setter per il footer
+        public function setFooter() {
+            // recupera il contenuto del footer (generico)
+            $this->_footer = file_get_contents(__DIR__."/content/common/_footer.html");
+            // sostituisce il contenuto di <footerPH></footerPH> (PlaceHolder) in $_whole_page
+            // con quello di $this->_footer
+            $this->_whole_page = str_replace("<footerPH></footerPH>", $this->_footer, $this->_whole_page);
+        }
+
+        // costruisce l'intera pagina
+        public function build() {
+            echo $this->_whole_page;
         }
     }
 ?>
