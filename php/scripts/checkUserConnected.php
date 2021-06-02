ed518c9be47bef19540c1fd028a703fbbfe0971b
AndreaBreggion <andreabreg99@gmail.com>
2021-06-03 01:43:13 +0200

    changed check user connected to not be a single link

--- php/scripts/checkUserConnected.php
+++ php/scripts/checkUserConnected.php
@@ -1,7 +1,7 @@
 <?php
   function checkUserConnection() {
     if(isset($_SESSION["uName"])) {
-      return('<span class="logHint"><a href="logout" role="button">Ciao ' . $_SESSION["uName"] . '! Effettua il logout</a></span>');
+      return('<span class="logHint">Ciao  ' . $_SESSION["uName"].'! ' .'<a href="php/logout.php" role="button">  Effettua il logout</a></span>');
     } else {
       return ('<span class="logHint"> Ciao! Non sei collegato (<a href="<rootDIR></rootDIR>/php/login.php" lang="en"> Login </a> o <a href="<rootDIR></rootDIR>/php/registrati.php"> Registrati </a>)</span>');
     }
