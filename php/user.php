f4618cc49d82b2a2746a0624f05cd5cc1cf26777
Sadi97 <andrea.salmaso@live.it>
2021-06-10 12:54:27 +0200

    Pagina utente (vuota) da riempire con form modifica

--- php/user.php
+++ php/user.php
@@ -1,13 +1,27 @@
 <?php
+session_start();
     require_once("../php/templateBuilder.php");
-
+    require_once('scripts/checkUserConnected.php');
+    require_once('scripts/createPresepeList.php');
+    require_once('scripts/connection.php');
+    require_once('scripts/lastVisitedPages.php');
+    handleVisitedPages('/php/user.php');
     // il parametro in input deve avere lo stesso nome del file che contiene tutto il codice html
-    $builder = new TemplateBuilder("users", "..");
+    $builder = new TemplateBuilder("/common/_pageTemplate", "..");
 
     $builder->setHead(file_get_contents(__DIR__."/content/common/_head.html"));
-    
-    $builder->setHeader(file_get_contents(__DIR__."/content/common/_header.html"));
-    $builder->setFooter(file_get_contents(__DIR__."/content/common/_footer.html"));
 
-    $builder->build();
+    $builder->setHeader(file_get_contents(__DIR__."/content/common/_header.html"), checkUserConnection());
+    $builder->setFooter(file_get_contents(__DIR__."/content/common/_footer.html"));
+    $builder->setBreadcrumb(file_get_contents(__DIR__."/content/common/_breadcrumbs.html"), array('<li class="current" aria-current="location">Utente</li>'));
+    $page = $builder->build();
+    $page = str_replace('<main id="content">', '<main id="content" class="mainUtente">', $page);
+    if(!isset($_SESSION['uId']) || $_SESSION["loggedin"]!='users') {
+        header('location: ../index.php');
+    }
+    else {
+        $page = str_replace('<placeholderContent></placeholderContent>', file_get_contents(__DIR__.'/content/user.html'), $page);
+    }
+    
+    echo($page);
 ?>
