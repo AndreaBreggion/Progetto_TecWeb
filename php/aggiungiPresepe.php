cba8b3539d1b32e87480f2b5d460ed17aac7b90c
AndreaBreggion <andreabreg99@gmail.com>
2021-06-10 02:47:39 +0200

    refactoring

--- php/aggiungiPresepe.php
+++ php/aggiungiPresepe.php
@@ -32,12 +32,12 @@
     if(empty($title)) $titleFinalResult = '<p class="errorMsg" tabindex="0">Campo obbligatorio</p>';
     if(strlen($title) < 3) $titleFinalResult = '<p class="errorMsg" tabindex="0">il titolo non può avere meno di 3 caratteri</p>';
     if(strlen($title) > 48) $titleFinalResult = '<p class="errorMsg" tabindex="0">il titolo non può avere più di 48 caratteri</p>';
-    if(preg_match('/[^a-zA-Z\d\s\.\!\?\,\;\:]/', $title)) $titleFinalResult = '<p class="errorMsg" tabindex="0">il titolo può contenere solo caratteri alfanumerici, spazi e punteggiatura</p>';
+    if(preg_match('/[^a-zA-Z\d\s\.\!\?\,\;\:\è\È\à\ò\ù\é]/', $title)) $titleFinalResult = '<p class="errorMsg" tabindex="0">il titolo può contenere solo caratteri alfanumerici, spazi e punteggiatura</p>';
 
     $description = trim($_POST['description']);
     $descFinalResult = '';
     if(empty($description)) $descFinalResult = '<p class="errorMsg" tabindex="0">Campo obbligatorio</p>';
-    if(preg_match('/[^a-zA-Z\d\s\.\!\?\,\;\:]/', $description)) $descFinalResult = '<p class="errorMsg" tabindex="0">La descrizione può contenere solo caratteri alfanumerici, spazi e punteggiatura</p>';
+    if(preg_match('/[^a-zA-Z\d\s\.\!\?\,\;\:\è\È\à\ò\ù\é]/', $description)) $descFinalResult = '<p class="errorMsg" tabindex="0">La descrizione può contenere solo caratteri alfanumerici, spazi e punteggiatura</p>';
 
     $category = $_POST['selectCategory'];
     $categoryFinalResult = '';
@@ -60,6 +60,10 @@
     if(empty($finalResult)) $isImageSaved = move_uploaded_file($_FILES['presepeImage']['tmp_name'], '../sources/images/'.$imageName);
     $date = date('Y-m-d');
     if(empty($finalResult) && $isImageSaved) {
+      $page = str_replace('titlePlaceholder', '', $page);
+      $page = str_replace('descriptionPlaceholder', '', $page);
+      $page = str_replace('adultiSelected', '', $page);
+      $page = str_replace('ragazziSelected', '', $page);
       $page = str_replace('<msgPlaceholder></msgPlaceholder>', '<p class="successMsg" tabindex="1"> Il tuo presepe è stato caricato!</p>', $page);
       $query = "INSERT INTO presepi (uId, photoPath, presepeName, category, description, dateOfCreation) VALUES (?,?,?,?,?,?);";
       $stmt = mysqli_stmt_init($connection);
--- php/register.php
+++ php/register.php
@@ -28,11 +28,11 @@ if(isset($_POST['submit'])) {
   if(strlen($username) < 3) $userNameFinalResult = '<p class="errorMsg" tabindex="0">lo username non può avere meno di 3 caratteri</p>';
   if(strlen($username) > 10) $userNameFinalResult = '<p class="errorMsg" tabindex="0">lo username non può avere più di dieci caratteri</p>';
   if(!preg_match('/^[a-zA-Z][a-zA-Z0-9.,$;]+$/', $username)) $userNameFinalResult = '<p class="errorMsg" tabindex="0">lo username deve contenere solo caratteri alfanumerici e iniziare con una lettera</p>';
-  $query = 'SELECT * FROM user WHERE username = ?';
+  $query = 'SELECT * FROM users WHERE username = ?';
   $isUserNameAlreadyTaken = statementQuery($connection, $username, $query);
   if($isUserNameAlreadyTaken) $userNameFinalResult = '<p class="errorMsg" tabindex="0">Questo username è già stato scelto!</p>';
   $mail = trim($_POST['mail']);
-  $query = 'SELECT * FROM user WHERE mail = ?';
+  $query = 'SELECT * FROM users WHERE mail = ?';
   $isMailAlreadyRegistered = statementQuery($connection, $mail, $query);
   $mailFinalResult = '';
   if($isMailAlreadyRegistered) $mailFinalResult = '<p class="errorMsg" tabindex="0">Questa mail è già stata registrata!</p>';
@@ -69,8 +69,13 @@ if(isset($_POST['submit'])) {
 
   $res = trim($nameFinalResult . $mailFinalResult . $surnameFinalResult . $passwordFinalResult . $userNameFinalResult);
   if(strlen($res) === 0) {
+    $page = str_replace('mailvaluePlaceholder', '', $page);
+    $page = str_replace('usernameValuePlaceholder', '', $page);
+    $page = str_replace('nameValuePlaceholder', '', $page);
+    $page = str_replace('surNameValuePlaceholder', '', $page);
+    $page = str_replace('passwordValuePlaceholder', '', $page);
     $password = password_hash($password, PASSWORD_DEFAULT);
-    $query = "INSERT INTO user (mail, name, surname, password, username) VALUES (?,?,?,?,?);";
+    $query = "INSERT INTO users (mail, name, surname, password, username) VALUES (?,?,?,?,?);";
     $stmt = mysqli_stmt_init($connection);
     mysqli_stmt_prepare($stmt, $query);
     mysqli_stmt_bind_param($stmt, "sssss", $mail , $name, $surname, $password, $username);
@@ -81,7 +86,7 @@ if(isset($_POST['submit'])) {
     $page = str_replace('mailvaluePlaceholder', htmlspecialchars($mail), $page);
     $page = str_replace('usernameValuePlaceholder', htmlspecialchars($username), $page);
     $page = str_replace('nameValuePlaceholder', htmlspecialchars($name), $page);
-    $page = str_replace('surnameValuePlaceholder', htmlspecialchars($surname), $page);
+    $page = str_replace('surNameValuePlaceholder', htmlspecialchars($surname), $page);
     $page = str_replace('passwordValuePlaceholder', htmlspecialchars($password), $page);
     $page = str_replace('<msgPlaceholder></msgPlaceholder>', '<p class="errorMsg" tabindex="1"> La registrazione non è andata a buon fine, ricontrolla i campi </p>', $page);
   }
