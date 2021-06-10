08380948903d8625979958e83c14c7f34d6eba52
AndreaBreggion <andreabreg99@gmail.com>
2021-06-10 02:22:43 +0200

    setting up heroku

--- php/scripts/connection.php
+++ php/scripts/connection.php
@@ -2,12 +2,12 @@
 
 function connect() {
 
-  $serverName = 'ec2-54-228-174-49.eu-west-1.compute.amazonaws.com:5432';
-  $username = 'bcbsuxzwykzrad';
-  $password = 'dd4e50fee47e6fd754895e93851a373869f5abdc2b7e9d8045104373af1b032e';
-  $db = 'ddt2oo41qdds8v';
+  $serverName = 'localhost:3306';
+  $username = 'root';
+  $password = 'Dalaskogen1444';
+  $db = 'tecweb';
 
-  $connection = mysqli_connect($serverName, $username, $password, $db);
+  $connection = mysqli_connect(parse_url(getenv('DATABASE_URL')));
   if (!$connection) {
     die("Connection failed: " . mysqli_connect_error());
   }
