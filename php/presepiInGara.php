a560013fa2c2b520ecef4e5ed4d6089bd8edb8dd
AndreaBreggion <andreabreg99@gmail.com>
2021-06-12 18:09:42 +0200

    hotfix

--- php/presepiInGara.php
+++ php/presepiInGara.php
@@ -13,16 +13,26 @@ $builder->setHead(file_get_contents(__DIR__."/content/common/_head.html"));
 
 $builder->setHeader(file_get_contents(__DIR__."/content/common/_header.html"), checkUserConnection());
 $builder->setFooter(file_get_contents(__DIR__."/content/common/_footer.html"));
-$builder->setBreadcrumb(file_get_contents(__DIR__."/content/common/_breadcrumbs.html"), array(
+if(isset($_GET['search'])){
+  $builder->setBreadcrumb(file_get_contents(__DIR__."/content/common/_breadcrumbs.html"), array(
+    '<li><a href="../index.php" lang="en">Home</a></li>',
+    '<li><a href="/php/presepiInGara.php">Presepi in gara</a></li>',
+    '<li class="current" aria-current="page"><span class="currentCrumb">Ricerca: '.htmlspecialchars($_GET['search']).'</span></li>'
+  ));
+} else $builder->setBreadcrumb(file_get_contents(__DIR__."/content/common/_breadcrumbs.html"), array(
   '<li><a href="../index.php" lang="en">Home</a></li>',
   '<li class="current" aria-current="page"><span class="currentCrumb">Presepi in gara</span></li>'
 ));
 
 $page = $builder->build();
-$page = str_replace('<li><a href="../php/presepiInGara.php">Presepi in Gara</a></li>', '<li class="current" aria-current="page"><span class="currentPage">Presepi in Gara</span></li>', $page);
+if(isset($_GET['search'])) {
+  $page = str_replace('<li><a href="../php/presepiInGara.php">Presepi in Gara</a></li>', '<li class="current" aria-current="page"><a href="../php/presepiInGara.php">Presepi in Gara</a></li>', $page);
+} else $page = str_replace('<li><a href="../php/presepiInGara.php">Presepi in Gara</a></li>', '<li class="current" aria-current="page"><span class="currentPage">Presepi in Gara</span></li>', $page);
 $page = str_replace('<main id="content">', '<main id="content" class="mainPresepi">', $page);
 $connection = connect();
-$replacement = createPresepeList($connection);
+$replacement = isset($_GET['search']) ? createPresepeSearchList($connection, $_GET['search']) : createPresepeList($connection);
+$replacement = strlen($replacement) == 0 ? '<h2 tabindex="1"><em> Errore 404: presepi non trovati! </em></h2>' : $replacement;
+$connection->close();
 $page = str_replace(' <placeholderContent></placeholderContent>', $replacement, $page);
 echo($page);
 ?>
--- php/scripts/createPresepeList.php
+++ php/scripts/createPresepeList.php
@@ -2,9 +2,9 @@
 
 function createPresepePost($row, $connection) {
     $post = file_get_contents(__DIR__.'/../content/common/_presepePost.html');
-    $post = str_replace('<authorPlaceHolder />', '<h4>'.$row['username'].'</h4>', $post);
+    $post = str_replace('<authorPlaceHolder />', '<h4>'.$row['presepeName'].'</h4>', $post);
     $post = str_replace('<datePlaceHolder />', '<p class="postDate">'.$row['dateOfCreation'].'</p>', $post);
-    $post = str_replace('<titlePlaceholder />', '<h5>'.$row['presepeName'] .' - categoria '. $row['category'].'</h5>', $post);
+    $post = str_replace('<titlePlaceholder />', '<h5>'.$row['username'] .' - categoria '. $row['category'].'</h5>', $post);
     $post = str_replace('<descriptionPlaceHolder />', '<p>'.$row['description'].'</p>', $post);
     $img = $row['photoPath'];
     $post = str_replace('<placeholderImage />', $img, $post);
@@ -24,5 +24,31 @@ function createPresepeList($connection) {
         $returnValue .= createPresepePost($row, $connection);
     }
     return($returnValue);
+}
+
+function createPresepeSearchList($connection, $where) {
+  $where = trim($where);
+  $query = 'SELECT presepi.*, users.username as username, users.id as UID from presepi INNER JOIN users on presepi.uId = users.id WHERE presepi.presepeName LIKE ?';
+  $stmt = mysqli_stmt_init($connection);
+  if( !mysqli_stmt_prepare($stmt, $query) ) {
+    exit;
+  }
+  $like = '%'.$where."%";
+  mysqli_stmt_bind_param($stmt, 's', $like);
+  mysqli_stmt_execute($stmt);
+  $result = mysqli_stmt_get_result($stmt);
+  mysqli_stmt_close($stmt);
+  $returnValue = '';
+  $i = 0;
+  while($row = mysqli_fetch_assoc($result)) {
+    $i++;
+    $returnValue .= createPresepePost($row, $connection);
+  }
+  if(strlen($returnValue) == 0) {
+    $returnValue = '<h2 tabindex="1"><em> La ricerca di '. htmlspecialchars($where) . ' non ha prodotto risultati!</em></h2>';
+  } else if($i == 1) {
+    $returnValue = '<p tabindex="0" class="searchResult"> La tua ricerca ha prodotto 1 risultato! </p>' . $returnValue;
+  } else $returnValue = '<p tabindex="0" class="searchResult"> La tua ricerca ha prodotto '.$i.' risultati! </p>' . $returnValue;
+  return($returnValue);
 }
 ?>
