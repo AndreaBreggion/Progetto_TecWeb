339b8ac8d18e7b5c1af9d8b7fe945ea88b9346eb
Alessandro Canel <alessandro99-c@live.com>
2021-06-18 19:17:39 +0200

    addRemoveSelected non modifica presepe corrente

--- php/scripts/addRemoveSelected.php
+++ php/scripts/addRemoveSelected.php
@@ -8,16 +8,17 @@ if(!isset($_SESSION['uId'])) {
     exit;
 } else {
     $connection = connect();
-    $query = "SELECT MAX uId FROM presepi WHERE Id = ".$_SESSION['lastVisitedPresepe'];
-    $result = $connection->query($query);//torna tutti i presepi dell'user
-    echo($result);
+    $query = "SELECT MAX uId as id FROM presepi WHERE Id = ".$_SESSION['lastVisitedPresepe'];//qui deve prendere il presepe della scheda
+    //  $query = 'SELECT uId as id FROM presepi WHERE id = '.$_SESSION['lastVisitedPresepe'];
+    $result = $connection->query($query);
+    //echo "<h1>".$result."</h1>";
     if(isset($_SESSION["uName"]) && $_SESSION["loggedin"]=='admin') {//se sei admin
         if (hasAdminSelected($connection, $result, $_SESSION['lastVisitedPresepe'])) {//se il presepe è vincitore
-            $query = 'UPDATE presepi SET winner=false WHERE uId= '. $result .' AND Id= ' . $_SESSION['lastVisitedPresepe'];
+            $query = "UPDATE presepi SET winner=false WHERE uId= '$result' AND Id= ".$_SESSION['lastVisitedPresepe'];
             $result = $connection->query($query);
             header('location: ../presepe.php?presepeId=' . $_SESSION['lastVisitedPresepe']);
         } else {//altrimenti
-            $query = "UPDATE presepi SET winner=false WHERE uId= '$result' AND Id= ".$_SESSION['lastVisitedPresepe'];
+            $query = "UPDATE presepi SET winner=true WHERE uId= '$result' AND Id= ".$_SESSION['lastVisitedPresepe'];
             $result = $connection->query($query);
             header('location: ../presepe.php?presepeId=' . $_SESSION['lastVisitedPresepe']);
         }
--- php/scripts/hasAdminSelected.php
+++ php/scripts/hasAdminSelected.php
@@ -1,6 +1,7 @@
 <?php
   function hasAdminSelected($connection, $uId, $id) {
-      $query = 'SELECT EXISTS (SELECT * FROM presepi WHERE uId = ".$uId."  AND pId = ".$id."  AND presepi.winner=1)';
+      $query = 'SELECT EXISTS (SELECT * FROM presepi WHERE uId = "$uId"  AND pId = "$id"  AND presepi.winner=1)';
+      //$connection->query("DELETE FROM users WHERE users.id = '$uID'");
       $result = $connection->query($query);
       if ($result) {
           //echo("Ok questo è vincitore");
