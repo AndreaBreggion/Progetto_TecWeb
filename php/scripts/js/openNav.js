8ea6bca83e6c72ff68c31981da248d0e2f3c9fda
AlessandroCanel <58002238+AlessandroCanel@users.noreply.github.com>
2021-06-22 18:04:38 +0200

    Risolto un errore che dava errori quando si scrollava su una pagina non presepi in gara

--- php/scripts/js/openNav.js
+++ php/scripts/js/openNav.js
@@ -2,7 +2,11 @@ window.onload=function jsAttivo() {
     document.getElementById("openImage").classList.add("jsActive");
     document.getElementById("closeImage").classList.add("jsActive");
     document.getElementById("menuContainer").classList.add("jsActive");
-    window.onscroll = function() {scrollFunction()};
+    if (document.getElementById("returnButton")) {
+        window.onscroll = function () {
+            scrollFunction()
+        };
+    }
 }
 
 function openNav(){
@@ -15,18 +19,19 @@ function openNav(){
     }
 }
 
-function scrollFunction() {
-    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-    //document.getElementById('returnButton').classList.add('Fade');
-    document.getElementById('returnButton').style.display = "block";
-    } else {
-    //document.getElementById('returnButton').classList.remove('Fade');
-    document.getElementById('returnButton').style.display = "none";
+
+    function scrollFunction() {
+        if (document.body.scrollTop > 150 || document.documentElement.scrollTop > 150) {
+            //document.getElementById('returnButton').classList.add('Fade');
+            document.getElementById('returnButton').style.display = "block";
+        } else {
+            //document.getElementById('returnButton').classList.remove('Fade');
+            document.getElementById('returnButton').style.display = "none";
+        }
     }
-}
 
     function topButton() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-}
+        document.body.scrollTop = 0;
+        document.documentElement.scrollTop = 0;
+    }
 
