ad0f311e298e600555e2f725bdfc571dc34f7d30
AlessandroCanel <58002238+AlessandroCanel@users.noreply.github.com>
2021-06-22 17:43:43 +0200

    separato tra js e html

--- php/scripts/js/openNav.js
+++ php/scripts/js/openNav.js
@@ -2,7 +2,7 @@ window.onload=function jsAttivo() {
     document.getElementById("openImage").classList.add("jsActive");
     document.getElementById("closeImage").classList.add("jsActive");
     document.getElementById("menuContainer").classList.add("jsActive");
-
+    window.onscroll = function() {scrollFunction()};
 }
 
 function openNav(){
@@ -14,3 +14,19 @@ function openNav(){
         menu.classList.remove("openMenu");
     }
 }
+
+function scrollFunction() {
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    //document.getElementById('returnButton').classList.add('Fade');
+    document.getElementById('returnButton').style.display = "block";
+    } else {
+    //document.getElementById('returnButton').classList.remove('Fade');
+    document.getElementById('returnButton').style.display = "none";
+    }
+}
+
+    function topButton() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
