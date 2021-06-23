96a7fae6d515b79a429ee79fd8cd1f1eee0dc1dd
Sadi97 <andrea.salmaso@live.it>
2021-06-23 18:01:56 +0200

    Aggiunto form filtro lista presepi

--- php/presepiInGara.php
+++ php/presepiInGara.php
@@ -34,9 +34,12 @@
   } else $page = str_replace('<li><a href="../php/presepiInGara.php">Presepi in Gara</a></li>', '<li class="current" aria-current="page"><span class="currentPage">Presepi in Gara</span></li>', $page);
   $page = str_replace('<main id="content">', '<main id="content" class="mainPresepi">', $page);
 
+  $filterForm = file_get_contents(__DIR__."/content/common/_filterForm.html");
   $page = str_replace('<placeholderContent />',
-                      '<h2 class="sectionTitle">Presepi attualmente in gara</h2><ul class="listaPresepi"><placeholderLista /></ul>
-                              <ul id="buttonTop"><placeholderButtonTop /></ul>', $page);
+                      '<h2 class="sectionTitle">Presepi attualmente in gara</h2>' .
+                      $filterForm .
+                      '<ul class="listaPresepi"><placeholderLista /></ul>
+                      <ul id="buttonTop"><placeholderButtonTop /></ul>', $page);
 
   $connection = connect();
   $replacement = isset($_GET['search']) ? createPresepeSearchList($connection, $_GET['search']) : createPresepeList($connection);
